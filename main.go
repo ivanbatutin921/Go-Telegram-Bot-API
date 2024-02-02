6de1c762450d7b9fea676a49d7b3094b451089ac
dopamine <ivanbatutin921@mail.ru>
2024-02-03 01:19:45 +0500

    03/02/2024|1:19

--- main.go
+++ main.go
@@ -3,20 +3,33 @@ package main
 import (
 	"log"
 	//"os"
+	//"strconv"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("6865656772:AAFBTWReSQgSEVBGUaTGultJ6xsoXfzeKVc") // use an environment variable for the token
+	botToken := ("6865656772:AAFBTWReSQgSEVBGUaTGultJ6xsoXfzeKVc")
+	if botToken == "" {
+		log.Fatal("TELEGRAM_BOT_TOKEN environment variable not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
-		log.Printf("Ошибка при запуске обоих : %s", err)
+		log.Printf("Error starting bot: %s", err)
 		return // don't panic, gracefully handle the error
 	}
 
 	bot.Debug = true
 
-	log.Printf("Авторизован на учетной записи %s", bot.Self.UserName)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	targetUsername := ("@morteit")
+	if targetUsername == "" {
+		log.Fatal("TELEGRAM_TARGET_USERNAME environment variable not set")
+	}
+
+	var targetUserID int64
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -24,15 +37,25 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // Если мы получим сообщение
+		if update.Message != nil { // If we receive a message
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
+			// Finding userID by username (assuming the bot has seen a message from the user)
+			if update.Message.From.UserName == targetUsername {
+				targetUserID = update.Message.From.ID
+			}
 
-			if _, err := bot.Send(msg); err != nil {
-				log.Printf("Сообщение об ошибке отправки: %s", err)
-				continue 
+			// Отправить сообщение указанному пользователю по имени пользователя
+			if targetUserID != 0 {
+				msg := tgbotapi.NewMessage(targetUserID, update.Message.Text)
+				msg.ReplyToMessageID = update.Message.MessageID
+
+				if _, err := bot.Send(msg); err != nil {
+					log.Printf("Error sending message: %s", err)
+					continue
+				}
+			} else {
+				log.Printf("Target user with username %s has not been seen by the bot yet.", targetUsername)
 			}
 		}
 	}
